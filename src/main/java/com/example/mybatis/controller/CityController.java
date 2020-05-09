e2ef3cc00546eceeedbc96a9bd45fa5714aee592
何伟 <hewei@unionbigdata.com>
2020-05-09 18:24:56 +0800

    Add swagger config

---
+++ src/main/java/com/example/mybatis/controller/CityController.java
@@ -0,0 +1,27 @@
+package com.example.mybatis.controller;
+
+import com.example.mybatis.domain.City;
+import com.example.mybatis.mapper.CityMapper;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/city")
+public class CityController {
+
+    private final CityMapper cityMapper;
+
+    public CityController(CityMapper cityMapper) {
+        this.cityMapper = cityMapper;
+    }
+
+    @GetMapping("/{id}")
+    public City show(@PathVariable Integer id) {
+        return cityMapper.selectByPrimaryKey(id);
+    }
+
+    @PostMapping
+    public City create(@RequestBody City city) {
+        cityMapper.insertSelective(city);
+        return city;
+    }
+}
